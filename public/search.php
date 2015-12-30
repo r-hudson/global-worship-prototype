2bfa5ff8341b9a04bfa3d2f7c20865ac4522a0d9
root <root@ip-172-31-25-1.us-west-2.compute.internal>
2015-12-30 21:54:14 +0000

    search.php has database connection implemented

--- public/search.php
+++ public/search.php
@@ -1,4 +1,30 @@
-<?php include "header.php"; ?>
+<?php 
+	include "header.php"; 
+	
+	$keyword=htmlspecialchars($_GET["keyword"]);
+	$artform=htmlspecialchars($_GET["art_form"]);
+	$language=htmlspecialchars($_GET["language"]);
+	$genre=htmlspecialchars($_GET["genre"]);
+
+	$dsn = 'mysql:host=globalmissions.c8fufuz88xej.us-west-2.rds.amazonaws.com;port=3306;dbname=ethnos';
+	$username = 'gwi';
+	$password = 'gwi-123!';
+
+	try {
+	    $dbh = new PDO($dsn, $user, $password);
+	} catch (PDOException $e) {
+	    echo 'Connection failed: ' . $e->getMessage();
+	}
+
+	if(empty($keyword) && empty($artform) && empty($language) && empty($genre)) {
+
+	} 
+	else {
+
+	}
+	
+
+?>
 	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 	<script type="text/javascript" src="js/search.js"></script>
 	<form action="search.php">
