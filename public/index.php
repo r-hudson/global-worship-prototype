cb222a0f9aeb3ba98c5cc7d16230076233d893ac
Ryan Mast <nightlark@users.noreply.github.com>
2015-12-30 12:26:55 -0800

    Update index.php to use bootstrap grid

--- public/index.php
+++ public/index.php
@@ -10,15 +10,15 @@
             <p>Harness the power of globalized technology to empower every nation, tribe, and tongue to experience and explore new worship expressions. </p>
         </div>
     </div>
-    
-    <div class="centered-content">
-
-        <div style="width: 500px; text-align: center;">
+    <div class="row">
+        <div class="col-sm6" style="text-align:right;">
             <a href="#search">
                 <div style="float:left;">
                     <img src="img/search-blue.png" alt="Search" />
                 </div>
             </a>
+        </div>
+        <div class="col-sm6" style="text-align:left;">
             <a href="#upload">
                 <div>
                     <img src="img/upload-blue.png" alt="Upload" />
