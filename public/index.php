0a490ab9d4ee31a5fa3bc63dc9692b83c3316915
Ryan Mast <nightlark@users.noreply.github.com>
2015-12-30 12:53:35 -0800

    Style update

--- public/index.php
+++ public/index.php
@@ -7,26 +7,26 @@
     </div>
     <div class="row">
         <div class="col-sm-12 centered-content">
-            <p>Harness the power of globalized technology to empower every nation, tribe, and tongue to experience and explore new worship expressions. </p>
+            <h3 class="homepage_motto">Harness the power of globalized technology to empower every nation, tribe, and tongue to experience and explore new worship expressions. </h3>
         </div>
     </div>
     <div class="row">
         <div class="col-sm-6" style="text-align:right;">
-            <div class="centered-content">
+            <div class="centered-content right-float">
                 <a href="#search">
                     <img src="img/search-blue.png" alt="Search" />
                 </a>
                 <br>
-                <button type="button" class="btn btn-default">Search</button>
+                <button type="button" class="btn btn-outlined">Search</button>
             </div>
         </div>
         <div class="col-sm-6" style="text-align:left;">
-            <div class="centered-content">
+            <div class="centered-content left-float">
                 <a href="#upload">
                     <img src="img/upload-blue.png" alt="Upload" />
                 </a>
                 <br>
-                <button type="button" class="btn btn-default">Upload</button>
+                <button type="button" class="btn btn-outlined">Upload</button>
             </div>
         </div>
     </div>
